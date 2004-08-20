885756b4e167b31d2b0098c466cf0370492b9b84
crazypops <crazypops@ae1140ef-c2e1-0310-b614-98986043f0fc>
2004-08-20 20:50:35 +0000

    bon gros cleanage d une sauvegarde fat32 a considere comme debut de projet a partir d ici

--- src/test.h
+++
@@ -1,5 +0,0 @@
-/* xmlparser.c */
-int parse_db(xmlDocPtr doc, xmlNodePtr cur, struct config *conf_struct);
-struct config *xmlparse(char *filename);
-int freeconf(struct config *conf);
-int main(int argc, char **argv);
